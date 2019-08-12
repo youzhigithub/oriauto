9b1bbaac8340c3b794627ef9b16754a6b46e01aa
suyz <3083467@gmail.com>
2019-08-12 21:01:25 +0800

    commit

--- service-provider/user-provider/src/main/java/com/ori/oriauto/user/UserProviderApplication.java
+++ service-provider/user-provider/src/main/java/com/ori/oriauto/user/UserProviderApplication.java
@@ -3,8 +3,6 @@ package com.ori.oriauto.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
--- service-provider/user-provider/src/main/java/com/ori/oriauto/user/controller/UserController.java
+++ service-provider/user-provider/src/main/java/com/ori/oriauto/user/controller/UserController.java
@@ -1,11 +1,21 @@
 package com.ori.oriauto.user.controller;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.indices.mapping.PutMapping;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController{
+
+    @Autowired
+    private JestClient jestClient;
 
     @RequestMapping("/test")
     public String test(){
